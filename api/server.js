8f0dd129eea966f77dbedd87241ca49e30e3bc3b
Richard Oh <n24512@protonmail.com>
2021-09-29 13:12:05 -0500

    server running, dependencies dl

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
 // global middlewares and the user's router need to be connected here
+server.use(express.json());
+server.use(helmet());
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
